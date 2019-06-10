f9750f3bd570972c3953879cd6d00df307203476
Daniel Sungju Kwon <sungju.kwon@gmail.com>
2019-06-10 15:45:30 +1000

    Change the page attribute of sys_call_table to modify

--- syscalls/mysyscalls.c
+++ syscalls/mysyscalls.c
@@ -9,7 +9,6 @@
 #include <linux/kallsyms.h>
 
 unsigned long syscall_addr;
-module_param(syscall_addr, ulong, 0);
 
 typedef long syscall_handler_t(void);
 syscall_handler_t **sys_call_table;
@@ -30,20 +29,31 @@ asmlinkage long myopen(const char __user *filename, int flags, umode_t mode)
   return orig_open(filename, flags, mode);
 }
 
+
+void (*set_pages_rw)(struct page *page, int numpages);
+
 int __init my_init(void)
 {
   int i;
-  if (syscall_addr == 0) {
-     syscall_addr = kallsyms_lookup_name("sys_call_table");
-  }
+  unsigned long set_pages_rw_addr;
+  struct page *sys_call_table_page;
+
+  write_cr0(read_cr0() & 0xfffeffff);
+
+  syscall_addr = kallsyms_lookup_name("sys_call_table");
   sys_call_table = (void *)syscall_addr;
+
+  /* Change the sys_call_tabel to be readable */
+  set_pages_rw_addr = kallsyms_lookup_name("set_pages_rw");
+  set_pages_rw = (void (*)(struct page*, int))set_pages_rw_addr;
+  sys_call_table_page = virt_to_page(sys_call_table);
+  set_pages_rw(sys_call_table_page, 1);
 
   for (i = 0; i < 20; i++) {
     printk("syscall[%i] = %p\n", i, sys_call_table[i]);
   }
   printk("open is at %d = %p\n", __NR_open, sys_call_table[__NR_open]);
   orig_open = (void *)sys_call_table[__NR_open];
-  /* Below can cause of crash in latest kernel as the table is protected */
   sys_call_table[__NR_open] = (syscall_handler_t *)myopen;
 
   return 0;
@@ -53,6 +63,7 @@ void __exit my_exit(void)
 {
   if (orig_open != NULL)
     sys_call_table[__NR_open] = (syscall_handler_t *)orig_open;
+  write_cr0(read_cr0() | (~0xfffeffff));
 }
 
 module_init(my_init);
